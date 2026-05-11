96df6a9eaddc377696460005d0f315d9547a46f3
Aditya Bajaj <159648482+AdityaB2731@users.noreply.github.com>
2026-05-12 00:35:58 +0530

    Update and rename assign2&3.txt to assign2.cpp

---
+++ assign2.cpp
@@ -0,0 +1,63 @@
+\#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+struct Node
+{
+    int vertex;
+    int g; // actual cost
+    int h; // heuristic cost
+    bool operator<(const Node &other) const
+    {
+        return (g + h) > (other.g + other.h);
+    }
+};
+int main()
+{
+    int n, e;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+    cout << "Enter number of edges: ";
+    cin >> e;
+    vector<vector<pair<int,int>>> adj(n);
+    cout << "Enter edges (source destination cost):\n";
+    for(int i = 0; i < e; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        adj[u].push_back({v, w});
+    }
+    vector<int> h(n);
+    cout << "Enter heuristic values:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> h[i];
+    }
+    int start, goal;
+    cout << "Enter start node: ";
+    cin >> start;
+    cout << "Enter goal node: ";
+    cin >> goal;
+    priority_queue<Node> pq;
+    pq.push({start, 0, h[start]});
+    cout << "\nA* Traversal Path:\n";
+    while(!pq.empty())
+    {
+        Node current = pq.top();
+        pq.pop();
+        cout << current.vertex << " ";
+        if(current.vertex == goal)
+        {
+            cout << "\nGoal Reached!\n";
+            break;
+        }
+        for(auto neighbour : adj[current.vertex])
+        {
+            int nextVertex = neighbour.first;
+            int edgeCost = neighbour.second;
+            pq.push({nextVertex,current.g + edgeCost,h[nextVertex]});
+        }
+    }
+    return 0;
+}
