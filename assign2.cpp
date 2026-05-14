239519c9e60d1d0bd3620639c31f43ce065cf50a
AdityaB2731 <aaditya31082005@gmail.com>
2026-05-14 22:51:29 +0530

    setup

--- assign2.cpp
+++ assign2.cpp
@@ -1,72 +1,62 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include<bits/stdc++.h>
 using namespace std;
-struct Node
-{
+struct Node{
     int vertex;
-    int g; // actual cost
-    int h; // heuristic cost
-    bool operator<(const Node &other) const
-    {
-        return (g + h) > (other.g + other.h);
+    int g;
+    int h;
+    bool operator<(const Node&other) const{
+        return (g+h)>(other.g+other.h);
     }
 };
-int main()
-{
+int main(){
     int n, e;
-    cout << "Enter number of vertices: ";
+    cout << "enter the number of vertices:";
     cin >> n;
-    cout << "Enter number of edges: ";
+    cout << "enter the number of edges:";
     cin >> e;
     vector<vector<pair<int, int>>> adj(n);
-    cout << "Enter edges (source destination cost):\n";
-    for (int i = 0; i < e; i++)
-    {
+    for (int i = 0; i < e; i++) {
         int u, v, w;
+        cout << "enter now(source,destination,cost):";
         cin >> u >> v >> w;
-
         adj[u].push_back({v, w});
     }
     vector<int> h(n);
-    cout << "Enter heuristic values:\n";
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
+        cout << "enter the heuristics for each vertex:";
         cin >> h[i];
     }
     int start, goal;
-    cout << "Enter start node: ";
+    cout << "enter the start node:";
     cin >> start;
-    cout << "Enter goal node: ";
+    cout << "enter the goal node:";
     cin >> goal;
-    priority_queue<Node> pq;
-    vector<bool> visited(n, false);
-    pq.push({start, 0, h[start]});
-    cout << "\nA* Traversal Path:\n";
-    while (!pq.empty())
-    {
+    priority_queue<Node>pq;
+    vector<bool>visited(n,false);
+    vector<int>g(n,1e9);
+    g[start]=0;
+    pq.push({start,0,h[start]});
+    cout<<"A* STAR PATH"<<endl;
+    while(!pq.empty()){
         Node current = pq.top();
         pq.pop();
-        if (visited[current.vertex])
-            continue;
-        visited[current.vertex] = true;
-        cout << current.vertex << " ";
-        if (current.vertex == goal)
-        {
-            cout << "\nGoal Reached!\n";
-            cout << "Total Cost = " << current.g;
+        if(visited[current.vertex]) continue;
+        visited[current.vertex]=true;
+        cout<<current.vertex<<"-";
+        if(current.vertex==goal){
+            cout<<endl;
+            cout<<"GOAL REACHED"<<endl;
+            cout<<"COST:"<<current.g<<endl;
             break;
         }
-        for (auto neighbour : adj[current.vertex])
-        {
-            int nextVertex = neighbour.first;
-            int edgeCost = neighbour.second;
-            if (!visited[nextVertex])
-            {
-                pq.push({nextVertex, current.g + edgeCost, h[nextVertex]});
+        for(auto x:adj[current.vertex]){
+            int next = x.first;
+            int wt = x.second;
+            int newval = wt+current.g;
+            if(g[next] >newval){
+                g[next]=newval;
+                pq.push({next,newval,h[next]});
             }
-            // pq.push({nextVertex,current.g + edgeCost,h[nextVertex]});
         }
     }
     return 0;
